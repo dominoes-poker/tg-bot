e1a1105c079b7631854da6fcb5e2eee82f21f27b
Artyom Tugaryov <artyom.tugaryov@gmail.com>
2022-06-28 16:10:53 +0300

    set bribrs for losers

--- bot/routers/game/round/bribes/handler.py
+++ bot/routers/game/round/bribes/handler.py
@@ -19,7 +19,7 @@ class BribesHandler(Handler):
         self._game_data_service = game_data_service
 
     async def ask_player_bribes(self, message: IncommingMessage,
-                                context_service: ContextService) -> None:
+                                context_service: ContextService) -> State:
         game_id = await context_service.get_current_game_id()
         game = await self._game_data_service.get_game(game_id)
         player = self._get_player_to_ask_results(game)
@@ -47,28 +47,29 @@ class BribesHandler(Handler):
         username = await context_service.from_whom_expect_bribe()
         player = next(filter(lambda p: p.username == username, game.players))
 
-        stake = dict(
-            playerId=player.id,
-            bribe = int(message.text),
-            roundId=game.last_round.number,
-        )
-        game = await self._game_data_service.set_bribe(game_id, stake)
+        await self._set_bribe(player.id, int(message.text), game)
 
         return await self.ask_player_bribes(message, context_service)
 
     async def _early_finish(self, game: Game, user_id: int) -> State:
         stakes = game.last_round.stakes
         players = {player.id: player.username for player in game.players}
-        losers = '`, `'.join([players[stake.playerId] for stake in stakes if stake.bribe is None])
+
+        losers = { 
+            stake.playerId: players[stake.playerId]
+            for stake in stakes if stake.bribe is None
+        }
+
+        for loser in losers:
+            await self._set_bribe(loser, 0, game)
+
+        losers = '`, `'.join(losers.values())
 
         await self._bot.send(
                 chat_id=user_id,
                 text=f'Looks like `{losers}` did not get anything in this round.'
         )
-
-    async def _early_finish(self, user_id: int) -> State:
         return await self._finish(user_id)
-
 
     async def _finish(self, user_id: int) -> State:
         await self._bot.send(
@@ -78,12 +79,23 @@ class BribesHandler(Handler):
         )
         return RoundState.STATISTICS
 
+    async def _set_bribe(self, player_id: int, bribe: int, game: Game) -> Game:
+        stake = dict(
+            playerId=player_id,
+            bribe = bribe,
+            roundId=game.last_round.number,
+        )
+        return await self._game_data_service.set_bribe(game.id, stake)
+
     def _get_bribes_variants(self, game: Game) -> Optional[List[int]]:
         stakes = sorted(game.last_round.stakes, key=lambda stake: stake.id)
         taken = sum(stake.bribe for stake in stakes if stake.bribe is not None)
+        
         remaining_bribes = get_number_of_dices(game, game.last_round.number) - taken
+        
         if remaining_bribes == 0:
             return None
+        
         return list(range(remaining_bribes + 1))
 
     def _get_player_to_ask_results(self, game: Game) -> Optional[Player]:
